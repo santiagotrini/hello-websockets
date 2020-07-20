2f693cbfc7a8a752a353d1febc594893028f54b5
Santiago Trini <santiagotrini@gmail.com>
2020-07-20 17:27:05 -0300

    no mas jquery por favor

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,15 +1,19 @@
-$(function () {
-  const socket = io();
-  $('form').submit(function(e){
-    e.preventDefault(); // prevents page reloading
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-  });
-  socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
-  });
+const socket = io();
+
+document.getElementById('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = document.getElementById('msg').value;
+  socket.emit('chat message', message);
+  document.getElementById('msg').value = '';
+  return false;
 });
 
-// TODO: refactor sacar jquery
+socket.on('chat message', (msg) => {
+  const li = document.createElement('li');
+  li.innerHTML = msg;
+  document.getElementById('messages').append(li);
+})
+
 // TODO: agregar nicknames
+// TODO: usar Bootstrap
+// TODO: agregar
