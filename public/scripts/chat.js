a8de092cca9dfb17a68e54dc98e097b164566157
Santiago Trini <santiagotrini@gmail.com>
2020-07-20 23:46:51 -0300

    agrega bootstrap

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -3,17 +3,18 @@ const socket = io();
 document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault();
   const message = document.getElementById('msg').value;
+  if (!message) return false;
   socket.emit('chat message', message);
   document.getElementById('msg').value = '';
   return false;
 });
 
 socket.on('chat message', (msg) => {
-  const li = document.createElement('li');
-  li.innerHTML = msg;
-  document.getElementById('messages').append(li);
+  const div = document.createElement('div');
+  div.innerHTML = msg;
+  div.classList.add('alert', 'alert-secondary')
+  document.getElementById('messages').append(div);
+  div.scrollIntoView();
 })
 
 // TODO: agregar nicknames
-// TODO: usar Bootstrap
-// TODO: agregar
